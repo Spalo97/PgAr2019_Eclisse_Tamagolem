26b96519b76a590f4c4b48346b599038bdc63f90
Giorgio <Giorgio@DESKTOP-KITJS2N>
2019-05-02 20:28:59 +0200

    Add pseudocode of balanca function

--- Tamagolem/src/TamagolemMain.java
+++ Tamagolem/src/TamagolemMain.java
@@ -2,8 +2,17 @@
 public class TamagolemMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		/*val=math.random*n*(Math.pow(-1, math.random*2));
+		
+		if val + allora prossimo -
+		
+		if somma totale +, allora prossimo negativo
+		
+		if somma totale -, allora prossimo positivo
+		
+		*/
 	}
 
 }

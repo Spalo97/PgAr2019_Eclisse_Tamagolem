b0025a789976babf8e15fea906a297d401dab214
Spalo97 <giorgio.spalenza@gmail.com>
2019-05-06 12:07:09 +0200

    Equilibrio quasi completo

--- Tamagolem/src/Equilibrio.java
+++ Tamagolem/src/Equilibrio.java
@@ -8,41 +8,58 @@ public class Equilibrio {
 
 	private int[][] equilibrio = new int[n][n];
 	
-	Random rand =new Random();
+	boolean check = false;
 	
 	public Equilibrio() {
 		for(int i=0;i<n;i++) {
+			somma=setSomma(equilibrio[i]);
 			for(int j=0;j<n;j++) {
-				if(i==j) {
-					equilibrio[i][j]=0;
-				}else if (j>i){
-					if(somma==0) {
-						equilibrio[i][j]=rand.nextInt(11);
-						equilibrio[j][i]=-equilibrio[i][j];
-						somma=somma+equilibrio[i][j];
-						break;
+					if(i==j) {
+						equilibrio[i][j]=0;
+					}else if(j>i)
+						if(j!=(n-1)) {
+							while(equilibrio[i][j]==0) {
+								if(somma==0 && check==false) {
+									equilibrio[i][j]=(int) (Math.random()*10);
+									equilibrio[j][i]=-equilibrio[i][j];
+									somma=somma+equilibrio[i][j];
+									check=true;
+								}
+								if(somma<0 && check==false) {
+									equilibrio[i][j]=(int) (Math.random()*10);
+									equilibrio[j][i]=-equilibrio[i][j];
+									somma=somma+equilibrio[i][j];
+									check=true;
+								}
+								if(somma>0 && check==false) {
+									equilibrio[i][j]=-(int) (Math.random()*10);
+									equilibrio[j][i]=-equilibrio[i][j];
+									somma=somma+equilibrio[i][j];
+									check=true;
+								}
+								check=false;
+							}
+						}else {
+							equilibrio[i][j]=-somma;
+							equilibrio[j][i]=somma;
 					}
-					if(somma<0) {
-						equilibrio[i][j]=rand.nextInt(somma);
-						equilibrio[j][i]=-equilibrio[i][j];
-						somma=somma+equilibrio[i][j];
-						break;
-					}
-					if(somma>0) {
-						equilibrio[i][j]=-rand.nextInt(somma);
-						equilibrio[j][i]=-equilibrio[i][j];
-						somma=somma+equilibrio[i][j];
-						break;
-					}
-				}
 			}
 		}
 	}
 	
+	private int setSomma(int row[]) {
+		int val=0;
+		for(int i=0;i<n;i++) {
+			val=val+row[i];
+		}
+		return val;
+	}
+	
+	
 	public void print() {
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
-				System.out.print(equilibrio[i][j]);
+				System.out.print(equilibrio[i][j]+" ");
 			}
 			System.out.println("");
 		}

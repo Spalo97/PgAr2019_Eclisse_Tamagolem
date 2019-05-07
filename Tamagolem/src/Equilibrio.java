ebc94a3e147bbe66c37808c5a8dcba4ddf5d559b
Spalo97 <giorgio.spalenza@gmail.com>
2019-05-07 19:08:29 +0200

    equilibrio

--- Tamagolem/src/Equilibrio.java
+++ Tamagolem/src/Equilibrio.java
@@ -18,32 +18,36 @@ public class Equilibrio {
 						equilibrio[i][j]=0;
 					}else if(j>i)
 						if(j!=(n-1)) {
-							while(equilibrio[i][j]==0) {
-								if(somma==0 && check==false) {
-									equilibrio[i][j]=(int) (Math.random()*10);
-									equilibrio[j][i]=-equilibrio[i][j];
-									somma=somma+equilibrio[i][j];
-									check=true;
-								}
-								if(somma<0 && check==false) {
-									equilibrio[i][j]=(int) (Math.random()*10);
-									equilibrio[j][i]=-equilibrio[i][j];
-									somma=somma+equilibrio[i][j];
-									check=true;
-								}
-								if(somma>0 && check==false) {
-									equilibrio[i][j]=-(int) (Math.random()*10);
-									equilibrio[j][i]=-equilibrio[i][j];
-									somma=somma+equilibrio[i][j];
-									check=true;
-								}
-								check=false;
-							}
+							elemento(i,j);
 						}else {
 							equilibrio[i][j]=-somma;
 							equilibrio[j][i]=somma;
 					}
 			}
+		}
+	}
+	
+	private void elemento(int i,int j) {
+		while(equilibrio[i][j]==0 || somma>10 || somma<-10) {
+			if(somma==0 && check==false) {
+				equilibrio[i][j]=(int) (Math.random()*10);
+				equilibrio[j][i]=-equilibrio[i][j];
+				somma=somma+equilibrio[i][j];
+				check=true;
+			}
+			if(somma<0 && check==false) {
+				equilibrio[i][j]=(int) (Math.random()*10);
+				equilibrio[j][i]=-equilibrio[i][j];
+				somma=somma+equilibrio[i][j];
+				check=true;
+			}
+			if(somma>0 && check==false) {
+				equilibrio[i][j]=-(int) (Math.random()*10);
+				equilibrio[j][i]=-equilibrio[i][j];
+				somma=somma+equilibrio[i][j];
+				check=true;
+			}
+			check=false;
 		}
 	}
 	
@@ -55,6 +59,27 @@ public class Equilibrio {
 		return val;
 	}
 	
+	private boolean controlloRiga(int i) {
+		int valRiga=0;
+		for(int j=0;j<n;j++) {
+			valRiga=valRiga+equilibrio[i][j];
+		}
+		if(valRiga<=10 || valRiga>=-10)
+			return true;
+		else
+			return false;
+	}
+	
+	private boolean controlloColonna(int j) {
+		int valColonna=0;
+		for(int i=0;i<n;i++) {
+			valColonna=valColonna+equilibrio[i][j];
+		}
+		if(valColonna<=10 || valColonna>=-10)
+			return true;
+		else
+			return false;
+	}
 	
 	public void print() {
 		for(int i=0;i<n;i++) {

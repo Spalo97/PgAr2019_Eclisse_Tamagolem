ccf3871593ab4f4cee1e6124250fd0db655bdf8d
Lange99 <c.langellotti@studenti.unibs.it>
2019-05-09 00:04:06 +0200

    main

--- Tamagolem/src/TamagolemMain.java
+++ Tamagolem/src/TamagolemMain.java
@@ -6,21 +6,13 @@ public class TamagolemMain {
 	public static void main(String[] args) {
 		
 		
-		/*val=math.random*n*(Math.pow(-1, math.random*2));
-		
-		if val + allora prossimo -
-		
-		if somma totale +, allora prossimo negativo
-		
-		if somma totale -, allora prossimo positivo
-		
-		*/
+
 		Battaglia partita = new Battaglia();
 		Supporto interazioni = new Supporto();
 
 		
 		
-		//interazioni.introduzione();
+		interazioni.introduzione();
 		n = interazioni.sceltaLivello();
 		partita.scontro(n);
 		

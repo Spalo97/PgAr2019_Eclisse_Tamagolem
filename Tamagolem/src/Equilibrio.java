ea8ec98ce3a73c3ad058b5483d654c15a28be852
Spalo97 <giorgio.spalenza@gmail.com>
2019-05-08 20:05:28 +0200

    ultima fase equilibrio

--- Tamagolem/src/Equilibrio.java
+++ Tamagolem/src/Equilibrio.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Equilibrio {
 
-	private int n;
+	private int n=5;
 	
 	private int somma = 0;
 
@@ -10,25 +10,51 @@ public class Equilibrio {
 	
 	boolean check = false;
 	
+	
 	public Equilibrio() {
-		for(int i=0;i<n;i++) {
-			somma=setSomma(equilibrio[i]);
-			for(int j=0;j<n;j++) {
-					if(i==j) {
-						equilibrio[i][j]=0;
-					}else if(j>i)
-						if(j!=(n-1)) {
-							elemento(i,j);
-						}else {
-							equilibrio[i][j]=-somma;
-							equilibrio[j][i]=somma;
+		
+			for(int i=0;i<n;i++) {
+				for(int j=0;j<n;j++) {
+						if(i==j) {
+							equilibrio[i][j]=0;					//diagonale di 0
+						}else if(j>i)
+							if(j!=(n-1)) {						//elementi sopra la diagonale
+								elemento(i,j);
+							}else{
+								if(somma!=0) {					//ultimo elemento della riga
+									equilibrio[i][j]=-somma;
+									equilibrio[j][i]=somma;
+								}else {
+									i=resetLinea(i);
+								}
+						}
+				}
+				if(i>=0) {
+					if(controlloMax(i)) {
+						i=resetLinea(i);
 					}
+				}
+				else if(i<(n-1)){
+					somma=setSomma(equilibrio[i+1]);
+				}
 			}
-		}
+	}
+	
+	private int resetLinea(int i) {
+		if(i==0) 
+			i--;
+		else
+			i=i-2;
+		
+		if(i<=0)
+			somma=0;
+		else
+			somma=setSomma(equilibrio[i]);			
+		return i;
 	}
 	
 	private void elemento(int i,int j) {
-		while(equilibrio[i][j]==0 || somma>10 || somma<-10) {
+		while(equilibrio[i][j]==0) {
 			if(somma==0 && check==false) {
 				equilibrio[i][j]=(int) (Math.random()*10);
 				equilibrio[j][i]=-equilibrio[i][j];
@@ -41,7 +67,6 @@ public class Equilibrio {
 				somma=somma+equilibrio[i][j];
 				check=true;
 			}
-			
 			if(somma>0 && check==false) {
 				equilibrio[i][j]=-(int) (Math.random()*10);
 				equilibrio[j][i]=-equilibrio[i][j];
@@ -59,27 +84,27 @@ public class Equilibrio {
 		}
 		return val;
 	}
-	
-	private boolean controlloRiga(int i) {
-		int valRiga=0;
+		
+	private boolean controlloMax(int i) {
+		int val=0;
+		boolean check=false;
 		for(int j=0;j<n;j++) {
-			valRiga=valRiga+equilibrio[i][j];
+			if(val<equilibrio[i][j]);
+				val=equilibrio[i][j];
 		}
-		if(valRiga<=10 || valRiga>=-10)
+		
+		if(val>10)
 			return true;
-		else
-			return false;
-	}
-	
-	private boolean controlloColonna(int j) {
-		int valColonna=0;
-		for(int i=0;i<n;i++) {
-			valColonna=valColonna+equilibrio[i][j];
+		
+		for(int j=0;j<n;j++) {
+			if(val>equilibrio[i][j]);
+				val=equilibrio[i][j];
 		}
-		if(valColonna<=10 || valColonna>=-10)
+		
+		if(val<-10)
 			return true;
-		else
-			return false;
+		
+		return false;
 	}
 	
 	public void print() {
